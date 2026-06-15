5287a89c293e161786c35c009a34de274cd684b0
mushkorr <kinyaj25@gmail.com>
2026-06-15 15:30:58 +0300

    Add JWT token routes

--- backend/kaeve/kaeve/urls.py
+++ backend/kaeve/kaeve/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 def api_root(request):
@@ -36,5 +37,8 @@ def api_root(request):
 urlpatterns = [
     path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include('core.urls')),
 ]
